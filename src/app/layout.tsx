4c1951bac580b93cc23ba8759efa4c938625cc30
Boris-Bodin <boris.bodin@gmail.com>
2024-04-16 13:06:16 +0200

    add avatar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,24 +2,57 @@ import React from 'react';
 import {Metadata} from 'next';
 
 import '@/styles/styles.scss';
-import QueryClientWrapper from '../shared/components/query-client-wrapper';
+import {Box, Container, AppBar, Button, Toolbar, Avatar} from '@mui/material';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
-    title: 'Nexus Nook',
-    description: 'Nexus Nook is a link-in-bio service',
+    title: 'Boris Bodin • Website',
+    description:
+        'Software Engineer. Web creator and educator about Code. I like to create things and help people who want to learn more and faster.',
 };
 
-export default function RootLayout({children}: { children: React.ReactNode }) {
+export default function RootLayout({children}: {children: React.ReactNode}) {
     return (
         <html lang='en'>
-        <head>
-            <meta charSet='utf-8'/>
-            <meta name='viewport' content='width=device-width, initial-scale=1'/>
-            <title>Nexus Nook</title>
-        </head>
-        <body suppressHydrationWarning={true}>
-        <QueryClientWrapper>{children}</QueryClientWrapper>
-        </body>
+            <head>
+                <meta charSet='utf-8' />
+                <meta name='viewport' content='width=device-width, initial-scale=1' />
+                {/*<meta name="robots" content="noindex, nofollow" />*/}
+                <link rel='icon' href='https://www.google.com/s2/favicons?sz=64&amp;domain_url=melvynx.com' />
+            </head>
+            <body suppressHydrationWarning={true}>
+                <AppBar position={'static'}>
+                    <Container maxWidth={false}>
+                        <Toolbar>
+                            <Box sx={{flexGrow: 1}}>
+                                <Avatar alt='Boris Bodin' src='/images/avatar.png' />
+                            </Box>
+
+                            <Box sx={{flexGrow: 0}}>
+                                <Link href={'/'}>
+                                    <Button color='inherit'>Home</Button>
+                                </Link>
+                                <Link href={'/'}>
+                                    <Button color='inherit'>Skills</Button>
+                                </Link>
+                                <Link href={'/'}>
+                                    <Button color='inherit'>Projects</Button>
+                                </Link>
+                                <Link href={'/'}>
+                                    <Button color='inherit'>Courses</Button>
+                                </Link>
+                                <Link href={'/'}>
+                                    <Button color='inherit'>Contact</Button>
+                                </Link>
+                                <Link href={'/'}>
+                                    <Button color='inherit'>Blogs</Button>
+                                </Link>
+                            </Box>
+                        </Toolbar>
+                    </Container>
+                </AppBar>
+                {children}
+            </body>
         </html>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
-import React from "react";
-import {AppBar, Button, Toolbar} from "@mui/material";
-import Link from "next/link";
+import React from 'react';
 
 export default function Page() {
-    return <>
-        <AppBar position={'static'}>
-            <Toolbar>
-                <Link href={'/'}>
-                    <Button color='inherit'>Home</Button>
-                </Link>
-            </Toolbar>
-        </AppBar>
-        <h1 className='underline'>Hello! Env : {process.env['NEXT_PUBLIC_ENVIRONNEMENT']}</h1>
-    </>;
+    return (
+        <>
+            <h1 className='underline'>Hello! Env : {process.env['NEXT_PUBLIC_ENVIRONNEMENT']}</h1>
+        </>
+    );
 }
